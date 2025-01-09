4385325e9bcbff9c9dab0442bbb4c4cbb85cd92e
jvera <jvera@cifpfbmoll.eu>
2025-01-09 09:49:32 +0100

    first commit

---
+++ app/src/main/java/com/example/splash_animated/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.splash_animated;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String GAME_PREFERENCES = "GamePrefs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.splash);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle(R.string.app_name);
+        setSupportActionBar(myToolbar);
+
+        Animation spinin = AnimationUtils.loadAnimation(this, R.anim.custom_anim);
+        LayoutAnimationController controller =
+                new LayoutAnimationController(spinin);
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
+
+        Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+        fade2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            public void onAnimationEnd(Animation animation) {
+                MainActivity.this.finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.clearAnimation();
+        }
+
+    }
+}
